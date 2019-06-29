0f0d2fa674d8e45a609037c3271e3d7ced7ebf35
Mydrean <2676030308@qq.com>
2019-06-30 00:02:21 +0800

    first commit

---
+++ js/Login.js
@@ -0,0 +1,13 @@
+$(function(){
+	
+	$('.login2').click(function(){
+		$('.box1').show();
+		$('.box2').hide();
+	})
+	$('.zibtn1').click(function(){
+		$('.box1').hide();
+		$('.box2').show();
+		
+		
+	})
+})
---
+++ js/shopCar2.js
@@ -0,0 +1,24 @@
+		//全选，但不知道为什么实现不了
+//		var tags=document.getElementsByTagName('input');
+//		var oNot=document.getElementById('#not')
+//		oNot.onclick=function(){
+//			alert("aaa")
+//				for(var i=0;i<tags.length;i++){
+//					tags[i].checked=!tags[i].checked;
+//				}
+//			}
+
+//		var oZjs=document.getElementById('zjs');
+//		document.onscroll=function(){
+//			var top=document.documentElement.scrollTop;
+//			if(top<1000){
+//					oZjs.style.position='fixed';
+//					oZjs.style.bottom=0;
+//					oZjs.style.z-index=50;
+//				}else{
+//					oZjs.style.position='static';
+//				}			
+//		}
+		
+
+		
